c7be72a4fafc778f80840f70ec1c73f0ac131471
bastenahamad <bastenahamad@gmail.com>
2023-10-02 02:14:53 +0530

    footer added

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import Logo from "./logo";
 import Home from "./homePage";
 import TopAnime from "./topAnime";
 import InfoPage from "./infoPage";
+import Footer from "./footer";
 import { Routes, Route} from "react-router-dom";
 import Search from "./search";
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/info" element={<InfoPage />} />
         <Route path="/search" element={<Search />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
